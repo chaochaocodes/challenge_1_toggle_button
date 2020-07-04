ea33883e7f9c5da19a1e1322d5932e28ddaffe5c
Alicia Chao <57464564+chaochaocodes@users.noreply.github.com>
2020-07-03 20:43:30 -0400

    pikachuOn

---
+++ src/components/Jump.js
@@ -0,0 +1,13 @@
+const jumpImages = [
+    { id: 1, src: '../media/01.png'},
+    { id: 2, src: '../media/02.png'},
+    { id: 3, src: '../media/03.png'},
+    { id: 4, src: '../media/04.png'},
+    { id: 5, src: '../media/05.png'},
+    { id: 6, src: '../media/06.png'},
+    { id: 7, src: '../media/07.png'},
+    { id: 8, src: '../media/08.png'},
+    { id: 9, src: '../media/09.png'}
+];
+
+export default jumpImages;
